f1047124c138e48bc8cd8133ca8d54d7ab5f246d
abderrahman amarnis <146735330+abderahmanamarnis@users.noreply.github.com>
2023-12-28 13:26:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-
-  var yourchoice = document.getElementById('yourchoice');
-      var selectedOption = document.getElementById('numberlist');
-      history = document.getElementById("history")
-
-      selectedOption.addEventListener('change', function(ev) {
-         console.log(ev.target.value);
-         result();
-      });
+let yourchoice = document.getElementById('yourchoice');
+let selectedOption = document.getElementById('numberlist');
+let history = document.getElementById("history")
 data = {}
-if(localStorage.length > 0){
-   data = {...JSON.parse(localStorage.getItem("key"))}
+if (localStorage.length > 0) {
+   data = { ...JSON.parse(localStorage.getItem("key")) }
 }
-      function result() {
-         calculationResult = parseInt(yourchoice.value) * parseInt(selectedOption.value)
-         console.log(parseInt(yourchoice.value) * parseInt(selectedOption.value));
-         data[`${selectedOption.value} * ${yourchoice.value}`] = calculationResult;
-         localStorage.setItem("key" , JSON.stringify(data));
-        
-         updateHistory()
-      };
-      
-      function updateHistory() {
-         let txt = "";
-         for (let x in data) {
-             txt += `${x} = ${data[x]}<br>`;
-         }
-         document.getElementById("history").innerHTML = txt;
-     }
-     
-     // Don't need this line, it doesn't do anything useful
-     // localStorage.getItem("key");
-     
-     updateHistory();
+function result() {
+   if(yourchoice.value!== ""&& selectedOption.value!== ""){
+   calculationResult = parseInt(yourchoice.value) * parseInt(selectedOption.value)
+   console.log(parseInt(yourchoice.value) * parseInt(selectedOption.value));
+   data[`${selectedOption.value} * ${yourchoice.value}`] = calculationResult;
+   localStorage.setItem("key", JSON.stringify(data));}
+   else{
+      alert("choose a number")
+   }
+   updateHistory()
+};
+function updateHistory() {
+   let txt = "";
+   for (let x in data) {
+      txt += `${x} = ${data[x]}<br>`;
+   }
+   document.getElementById("history").innerHTML = txt;
+}
+updateHistory();
